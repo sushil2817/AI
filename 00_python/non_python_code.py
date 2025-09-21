2e795bc89454ba5b0c67b9d44d44a7342a6f4e66
sushil2817 <tony281723@gmail.com>
2025-09-21 08:05:14 +0530

    Remove unnecessary whitespace in make_chai function for improved readability

--- 00_python/non_python_code.py
+++ 00_python/non_python_code.py
@@ -2,21 +2,13 @@ def make_chai():
     if not kettle_has_water():
         fill_kettle()
     plug_in_kettle()
-    
     boil_water()
-
     if not us_cup_clean():
-
         was_cup()
-
     add_to_cup("tea_leaves") 
-
     add_to_cup("sugar")
-
     pour("boiled water")
-
     stie("cup")
-    
     serve("chai")        
 
 
