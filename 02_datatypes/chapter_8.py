b0952430b0880be804cf688199ce1b8b9a1d331f
sushil2817 <tony281723@gmail.com>
2025-09-28 19:58:17 +0530

    Remove unnecessary whitespace for improved readability and add newline at end of file

--- 02_datatypes/chapter_8.py
+++ 02_datatypes/chapter_8.py
@@ -1,25 +1,20 @@
 ingredients = ["water","milk","black tea"]
-
 ingredients.append("sugar")
 print(f"Ingredients are: {ingredients}")
 ingredients.remove("water")
 print(f"Ingredients are: {ingredients}")
-
 spice_options = ["ginger","cardamom"]
 chai_ingredients = ["water", "milk"]
-
 chai_ingredients.extend(spice_options)
 print(f"Chai: {chai_ingredients}")
 chai_ingredients.insert(2,"black tea")
 print(f"Chai: {chai_ingredients}")
-
 last_added = chai_ingredients.pop()
 print(f"Last: {last_added}")
 chai_ingredients.reverse()
 print(f"Chai: {chai_ingredients}")
 chai_ingredients.sort()
 print(f"Chai: {chai_ingredients}")
-
 sugar_level = [0,1,2,3,4,5,6]
 print(f"Maximum sugar level: {max(sugar_level)}")
 print(f"Maximum sugar level: {min(sugar_level)}")
