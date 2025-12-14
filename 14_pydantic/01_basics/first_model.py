fc9d931398acfda5ae5c8eb6489cb75d6a821455
sushil2817 <tony281723@gmail.com>
2025-12-14 08:07:23 +0530

    Learn about pydantic and Base Model

---
+++ 14_pydantic/01_basics/first_model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id:int
+    name:str
+    is_active:bool
+input_data = {
+    'id':'101',
+    'name':"Chaicode",
+    'is_active':True
+    }
+user = User(**input_data)
+print(user)
