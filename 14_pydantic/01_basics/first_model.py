c28c3b18b83e60171f5b27e87a93af0df4f715b4
sushil2817 <tony281723@gmail.com>
2026-01-10 12:35:37 +0530

    changes

--- 14_pydantic/01_basics/first_model.py
+++ 14_pydantic/01_basics/first_model.py
@@ -3,10 +3,12 @@ class User(BaseModel):
     id:int
     name:str
     is_active:bool
+    
 input_data = {
     'id':'101',
     'name':"Chaicode",
     'is_active':True
     }
+
 user = User(**input_data)
 print(user)
