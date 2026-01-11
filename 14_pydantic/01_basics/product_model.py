3ac32e26d706999088dc478bec292795cf1b2566
sushil2817 <tony281723@gmail.com>
2026-01-11 12:35:56 +0530

    new changes

--- 14_pydantic/01_basics/product_model.py
+++ 14_pydantic/01_basics/product_model.py
@@ -5,9 +5,6 @@ class Product(BaseModel):
     name:str
     price:float
     in_stock:bool=True
-
 product_one = Product(id=1,name="Laptop",price=999.99,in_stock=True)
-
 product_two = Product(id=2,name="Mouse",price=600,in_stock=False)
-
 product_three = Product(name="keyboard")
