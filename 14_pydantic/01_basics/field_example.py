cee6928e43377c60c9b3b233db35295353c68c65
sushil2817 <tony281723@gmail.com>
2026-01-18 04:12:09 +0530

    fix: add missing newlines for better readability in field_example.py

--- 14_pydantic/01_basics/field_example.py
+++ 14_pydantic/01_basics/field_example.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 from typing import List, Dict,Optional
+
 class Cart(BaseModel):
     user_id:int
     items:List[str]
     quantities: Dict[str,int]
+
 class BlogPost(BaseModel):
     title:str
     content:str
