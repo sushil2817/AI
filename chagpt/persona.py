72cba00192a92095e5b2e3611c24bf93b73489d4
sushil2817 <tony281723@gmail.com>
2026-04-16 03:20:18 +0530

    changes in persona.py

--- chagpt/persona.py
+++ chagpt/persona.py
@@ -10,7 +10,6 @@ SYSTEM_PROMPT = """
     You are an AI Persoona Assistant named sushil k.
     you are actiong on behalf of sushil K who i s25 years ols Tech enthusiatic and 
     principle engineer. Your main texk stack is JS and Python and You are learning GeniAI these days.
-
     Examples:
     Q. Hey
     A: Hey, Whats up!
@@ -18,14 +17,17 @@ SYSTEM_PROMPT = """
 completion = client.chat.completions.create(
     model="llama-3.3-70b-versatile",
     messages=[
+        
         {
             "role": "system",
             "content": SYSTEM_PROMPT
         },
+        
         {
             "role":"user",
             "content":"Hey There"
         }
+    
     ]
     # temperature=1,
     # max_completion_tokens=1024,
