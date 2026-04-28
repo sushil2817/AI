d32b1f6165335fc584dcbb6e3b14b74c79da0a10
sushil2817 <tony281723@gmail.com>
2026-04-28 23:23:14 +0530

    fix: correct indentation in chai_price_calculator.py

--- 03_conditionals/chai_price_calculator.py
+++ 03_conditionals/chai_price_calculator.py
@@ -1,8 +1,10 @@
 cup = input("Choose your cup size (small/medium/large): ").lower()
 
 if cup == "small":
+
     print("Price is 10 rupess")
 elif cup == "medium":
+    
     print("Price is 15 rupees")
 elif cup == "large":
     print("Price is 20 rupees")
