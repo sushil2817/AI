58a2c6d55647e829fd439238d6498e7f74b34fee
sushil2817 <tony281723@gmail.com>
2026-05-01 22:29:02 +0530

    fix: remove unnecessary blank lines in chai_price_calculator.py

--- 03_conditionals/chai_price_calculator.py
+++ 03_conditionals/chai_price_calculator.py
@@ -1,10 +1,7 @@
 cup = input("Choose your cup size (small/medium/large): ").lower()
-
 if cup == "small":
-
     print("Price is 10 rupess")
 elif cup == "medium":
-    
     print("Price is 15 rupees")
 elif cup == "large":
     print("Price is 20 rupees")
