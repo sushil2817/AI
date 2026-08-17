09619ab9140393c5e026319546ac7a632433baaa
sushil2817 <tony281723@g.gmail.com>
2026-08-17 19:44:59 +0530

    duplicate function

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,10 @@
+def print_order(name,chai_type):
+    print(f"{name} orderded {chai_type} chai!")
+
+
+
+print_order("Sushil","masala")
+print_order("Nisha","Ginger")
+print_order("Lubdha","Tulsi")
+
+# print("sushil")
