f99d68ba8e76cf9a772f0af16728f7147d8f9d63
sushil2817 <tony281723@g.gmail.com>
2026-08-20 05:46:33 +0530

    hiding

---
+++ 05_functions/03_hiding.py
@@ -0,0 +1,16 @@
+def get_input():
+    print("Getting user input")
+
+def validate_input():
+    print("Validationg the user info")
+
+def save_to_db():
+    print("saving to database")
+
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("User registration complete")
+
+register_user()
