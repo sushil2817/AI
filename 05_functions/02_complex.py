676ec6272bab41ef56b41529d12229979d93cd4c
sushil2817 <tony281723@gmail.com>
2026-09-12 23:26:14 +0530

    create some functions

--- 05_functions/02_complex.py
+++ 05_functions/02_complex.py
@@ -1,17 +1,12 @@
 def fetch_sales():
     print("Fetching the sales data")
-
-
 def filter_valid_sales():
     print("Filtering valid sales data")
-
 def summarize_data():
     print("Summaring sales data")
-
 def generate_report():
     fetch_sales()
     filter_valid_sales()
     summarize_data()
     print("Report is ready")
-
 generate_report()
