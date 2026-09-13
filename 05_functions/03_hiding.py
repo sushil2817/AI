6b3ceab1b201b03152d78e5979a96ad2c12efc65
sushil2817 <tony281723@gmail.com>
2026-09-13 19:26:53 +0530

    pratcie some more functions

--- 05_functions/03_hiding.py
+++ 05_functions/03_hiding.py
@@ -1,12 +1,9 @@
 def get_input():
     print("Getting user input")
-
 def validate_input():
     print("Validationg the user info")
-
 def save_to_db():
     print("saving to database")
-
 def register_user():
     get_input()
     validate_input()
--- 05_functions/06_scopes.py
+++ 05_functions/06_scopes.py
@@ -1,11 +1,9 @@
 def serve_chai():
     chai_type = "Masala"
     print(f"Inside function: {chai_type}")
-
 chai_type = "Lemon"
 # serve_chai()
 # print(f"Outside function: {chai_type}")
-
 def chai_counter():
     chai_order = "lemon"
     def print_order():
@@ -13,7 +11,6 @@ def chai_counter():
         print("Inner: ",chai_order)
     print("Outer: ",chai_order)
     print_order()
-
 chai_counter()
 print("code running")
 
